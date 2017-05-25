688341c041fa4d794dbf6b680337f0b96fa3e144
PetrenkoS <petrenko.s@yahoo.com>
2017-05-25 22:32:42 +0300

    Попытка создать функцию вычисления расстояния
    Просьба проверить, что не так. Была в командировке 2 недели, а сейчас
    сижу над этим заданием уже 4 часа и не могу сделать :(

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,25 +3,36 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 
+    public MyFirstProgram() {
+            }
+
     public static void main(String[] args) {
         hello("world");
         hello("user");
         hello("Svetlana");
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+                System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
-    public static double area(double len) {
-        return len * len;
+    public static double area(Square s) {
+        return s.l * s.l;
     }
-    public static double area(double a, double b) {
-        return a * b;
+
     }
-}
+
+
+    public static void main(String[] args) {
+        Point p1 = new Point(6, 2);
+        Point p2 = new Point(4, 4);
+
+        double c = Math.sqrt(p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
+
+        System.out.println(с);
+    }
